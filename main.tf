7aa85a0d5ada700432938517198839bd1f086193
Pamela Luque <pamela_luque@mckinsey.com>
2023-11-10 07:21:03 -0500

    feat: rg & vnet & acr

--- main.tf
+++ main.tf
@@ -1,4 +1,23 @@
-resource "azurerm_resource_group" "default_rg" {
-  name     = "default-resource-group"
-  location = "westus2"
+# resource group
+resource "azurerm_resource_group" "az-capabilities-rg" {
+  name     = "az-capabilities-rg"
+  location = "eastus2"
+}
+
+# vnet
+resource "azurerm_virtual_network" "chupito-vnet-1" {
+  name = "az-capabilities-vnet-1"
+  location = "eastus2"
+  resource_group_name = "az-capabilities-rg"
+  address_space = ["10.0.0.0/16"]
+}
+
+#azure container registry
+resource "azurerm_container_registry" "containerregistrychupito" {
+  name                     = "containerregistrychupito"
+  resource_group_name      = "az-capabilities-rg"
+  location                 = "eastus2"
+  admin_enabled            = true
+  sku                      = "Standard"
+  
 }
