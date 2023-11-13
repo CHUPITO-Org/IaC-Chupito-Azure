889cd805fab00e427450b881f3883fab421b218b
Pablo Ramirez <pablo_a_ramirez@mckinsey.com>
2023-11-13 19:02:23 -0300

    test(tf modules): test if the providers config is right

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,18 +1,3 @@
-# vnet/main.tf
-terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.0.1"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_virtual_network" "vnet-module" {
   name                = var.vnet_name
   address_space       = var.address_space
