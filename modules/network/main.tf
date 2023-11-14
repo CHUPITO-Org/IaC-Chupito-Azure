f4e947ecc1aba1357fec69973da37eb31b6df62a
Pamela Luque <pamela_luque@mckinsey.com>
2023-11-14 11:40:02 -0500

    fix(modules): remove providers from network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,18 +1,4 @@
 # vnet/main.tf
-terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.0.1"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = var.address_space
