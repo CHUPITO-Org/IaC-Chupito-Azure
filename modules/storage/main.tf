6e9cf10b7f4f7b9a1fcfe6e1efd8c4a78e07c7ff
pamelaluquemck <pamela_luque@mckinsey.com>
2023-11-29 11:10:30 -0500

    feat(container): add database container with intialize values and volume

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,14 +1,30 @@
+# Storage Account
 resource "azurerm_storage_account" "storage" {
-  name                     = "storagechupito"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  min_tls_version          = "TLS1_2"
+  name                            = "storagedb"
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = true
 }
 
+# File share to container volume
 resource "azurerm_storage_share" "storage_share" {
-  name                 = "fschupito"
+  name                 = "fsdb"
   storage_account_name = azurerm_storage_account.storage.name
   quota                = 50
 }
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
+output "storage_account_key" {
+  value = azurerm_storage_account.storage.primary_access_key
+}
+
+output "share_name" {
+  value = azurerm_storage_share.storage_share.name
+}
+
